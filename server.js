c03d7111bffdacb0f64d6a6cea20824bca20ea68
unknown <anuj.khemka.us@gmail.com>
2022-10-16 10:02:05 -0500

    changed port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('dotenv').config({ path: './config.env' });
 
 const app = express();
 const DATABASE_CONNECTION = process.env.ATLAS_URI;
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 console.log(DATABASE_CONNECTION)
 
