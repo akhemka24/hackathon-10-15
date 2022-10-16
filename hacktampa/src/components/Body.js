56fc474e299ea92332dded713c2c6eea575815e8
unknown <henriquegamonal3@gmail.com>
2022-10-16 10:51:15 -0400

    Added fetch request and got data from cognigy.ai

---
+++ hacktampa/src/components/Body.js
@@ -0,0 +1,28 @@
+import "./Body.css"
+
+function Body() {
+    return (
+        <div>
+            <div className="body-description">
+                <div className="brand-call">
+                    <div className= "brand">
+                        <h1>
+                            Mental Solutions
+                        </h1>
+                    </div>
+                    <div className="call-for-action">
+                        <h2>Click here to start chatting</h2>
+                        <img></img>
+                    </div>
+                </div>
+                <div className="description">
+                    <p>We are here to help you with your mental health. Start chatting and so we can help you or help you psychologist solve your problem</p>
+                </div>
+            </div>
+            
+            
+        </div>
+    )
+}
+
+export default Body
--- hacktampa/src/index.js
+++ hacktampa/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from "axios"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +10,24 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+let data;
+document.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    setTimeout(function(){
+      for (const div of document.querySelectorAll("div")) {
+        if (div.textContent.includes("date") && div.textContent[0] == '{') {
+          data = div.textContent;
+          const obj = JSON.parse(data);
+          fetch('http://localhost:3000/mentalDb/', {
+            method: 'POST',
+            body: obj
+        })
+          console.log(obj);
+        }
+      }
+  }, 2000);
+    }
+});
+
+
