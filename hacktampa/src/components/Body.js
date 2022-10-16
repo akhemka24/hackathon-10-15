b6a0cb17cd74ab428f1ce11073ee8b1ee879be69
Gamonal6 <59625543+Gamonal6@users.noreply.github.com>
2022-10-16 12:37:02 -0400

    Update Body.js

--- hacktampa/src/components/Body.js
+++ hacktampa/src/components/Body.js
@@ -10,10 +10,7 @@ function Body() {
                             Mental Solutions
                         </h1>
                     </div>
-                    <div className="call-for-action">
-                        <h2>Click here to start chatting</h2>
-                        <img></img>
-                    </div>
+       
                 </div>
                 <div className="description">
                     <p>We are here to help you with your mental health. Start chatting and so we can help you or help you psychologist solve your problem</p>
